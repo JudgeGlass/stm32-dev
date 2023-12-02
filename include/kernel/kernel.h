aac634d1d83a997711e32d519d603b23ca4d4615
Hunter Wilcox <hunter.wilcox@judgeglass.net>
2023-12-02 11:53:12 -0600

    move to kernel

--- include/kernel/kernel.h
+++ include/kernel/kernel.h
@@ -22,4 +22,12 @@ void k_print_msg(char *m_msg);
 
 static uint32_t panic();
 
+static inline uint8_t timer_expired(uint32_t *t, uint32_t prd, uint32_t now) {
+  if (now + prd < *t) *t = 0;
+  if (*t == 0) *t = now + prd;
+  if (*t > now) return 0;
+  *t = (now - *t) > prd ? now + prd : *t + prd;
+  return 1;
+} 
+
 #endif
--- src/main.c
+++ src/main.c
@@ -4,15 +4,6 @@
 
 #include <kernel/kernel.h>
 
-uint8_t timer_expired(uint32_t *t, uint32_t prd, uint32_t now) {
-  if (now + prd < *t) *t = 0;                    // Time wrapped? Reset timer
-  if (*t == 0) *t = now + prd;                   // Firt poll? Set expiration
-  if (*t > now) return 0;                    // Not expired yet, return
-  *t = (now - *t) > prd ? now + prd : *t + prd;  // Next expiration time
-  return 1;                                   // Expired, return true
-} 
-
-
 int main(void) {
     systick_init(16000000 / 1000);
     k_init();
